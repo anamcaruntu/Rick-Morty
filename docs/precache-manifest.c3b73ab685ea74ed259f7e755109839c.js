7de214ae5d4b01ee20f97fce187ddcde0b75acd6
Ana Maria Caruntu <anam.caruntu@gmail.com>
2020-08-10 11:54:04 +0200

    docs

---
+++ docs/precache-manifest.c3b73ab685ea74ed259f7e755109839c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860cf36dc9577d5a51ea6bd8ee1fc717",
+    "url": "./index.html"
+  },
+  {
+    "revision": "589556b0f3114b5b69c1",
+    "url": "./static/css/main.861a7b9b.chunk.css"
+  },
+  {
+    "revision": "b3e0dd60b6f46ebb524e",
+    "url": "./static/js/2.93f86f9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.93f86f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589556b0f3114b5b69c1",
+    "url": "./static/js/main.be184130.chunk.js"
+  },
+  {
+    "revision": "57de9c66652350fd951d",
+    "url": "./static/js/runtime-main.59d9451b.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick_and_Morty.75509f2e.png"
+  }
+]);
